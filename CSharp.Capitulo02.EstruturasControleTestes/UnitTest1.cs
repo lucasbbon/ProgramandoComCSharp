f38db250f50387981f2ba9b7aa492b1c46e15a5b
Lucas <blessedsovereign@gmail.com>
2021-04-20 22:47:15 -0300

    Tabuada teste

---
+++ CSharp.Capitulo02.EstruturasControleTestes/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharp.Capitulo02.EstruturasControleTestes
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TabuadaTeste()
+        {
+        }
+    }
+}
