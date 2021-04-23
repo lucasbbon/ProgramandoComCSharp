397e8f5638e9f2899266ca2b5ce515308da15927
Lucas <blessedsovereign@gmail.com>
2021-04-22 22:53:04 -0300

    Testes e Fintech

---
+++ CSharp.Capitulo02.EstruturasControleTestes/DecisãoTeste.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+
+namespace CSharp.Capitulo02.EstruturasControleTestes
+{
+    [TestClass]
+    public class DecisãoTeste
+    {
+        [TestMethod]
+        public void AvaliaçãoFinalReprovado29Teste()
+        {
+            double notaFinal = 2.9;
+            string resultadoFinal = ""; //notaFinal < 3 ? "Reprovado" : notaFInal >= 3 && notaFinal < 5 ? "Recuperação" : "Aprovado";
+
+            if (notaFinal < 3)
+            {
+                resultadoFinal = "Reprovado";
+            }
+            else if (notaFinal is >= 3 and < 5)
+            {
+                resultadoFinal = "Recuperação";
+            }
+            else
+            {
+                resultadoFinal = "Aprovado";
+            }
+
+            /*switch(notaFinal)
+            {
+                case < 3:
+                    resultadoFinal = "Reprovado";
+                    break;
+                case >= 3 and < 5:
+                    resultadoFinal = "Recuperação";
+                    break;
+                default:
+                    resultadoFinal = "Aprovado";
+                    break;
+            }*/
+
+            Assert.AreEqual(resultadoFinal, "Reprovado'");
+
+            Console.WriteLine(resultadoFinal);
+        }
+
+        [TestMethod]
+        public void AvaliaçãoFinalRecuperação3Teste()
+        {
+            double notaFinal = 3.0;
+            string resultadoFinal = "";
+
+            switch (notaFinal)
+            {
+                case < 3:
+                    resultadoFinal = "Reprovado";
+                    break;
+                case >= 3 and < 5:
+                    resultadoFinal = "Recuperação";
+                    break;
+                default:
+                    resultadoFinal = "Aprovado";
+                    break;
+            }
+
+            Assert.AreEqual(resultadoFinal, "Recuperação");
+        }
+
+        [TestMethod]
+        public void AvaliaçãoFinalRecuperação49Teste()
+        {
+            double notaFinal = 4.9;
+            string resultadoFinal = notaFinal switch
+            {
+                < 3 => "Reprovado",
+                >= 3 and < 5 => "Recuperação",
+                _ => "Aprovado",
+            };
+
+            Assert.AreEqual(resultadoFinal, "Recuperação");
+        }
+
+        [TestMethod]
+        [DataRow(2.9, "Reprovado")]
+        [DataRow(3, "Recuperação")]
+        [DataRow(4.9, "Recuperação")]
+        [DataRow(5, "Aprovado")]
+        public void AvaliaçãoFinalTeste(double notaFinal, string resultadoEsperado)
+        {
+            string resultadoFinal = notaFinal switch
+            {
+                < 3 => "Reprovado",
+                >= 3 and < 5 => "Recuperação",
+                _ => "Aprovado",
+            };
+
+            Assert.AreEqual(resultadoFinal, resultadoEsperado);
+        }
+    }
+}
---
+++ Fintech.Modelos/ContaCorrente.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Fintech.Modelos
+{
+    public class ContaCorrente
+    {
+
+    }
+}
